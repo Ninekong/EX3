7e15728949278012105c3a041906fd151b5b2f18
Ninekong <122553014+Ninekong@users.noreply.github.com>
2023-05-15 16:03:28 +0700

    Add files via upload

---
+++ EXx3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+class ImageProcessing
+{
+    static void Main()
+    {
+        
+        Console.Write("filelocation: ");
+        string inputImagePath = Console.ReadLine();
+
+        
+        Console.Write("filelocation: ");
+        string kernelPath = Console.ReadLine();
+
+        
+        Console.Write("filelocationinfo: ");
+        string outputImagePath = Console.ReadLine();
+
+        
+        double[,] imageData = ReadImageDataFromFile(inputImagePath);
+
+        
+        double[,] kernel = ReadImageDataFromFile(kernelPath);
+
+       
+        double[,] result = Convolve(imageData, kernel);
+
+        
+        WriteImageDataToFile(outputImagePath, result);
+
+        
+    }
+
+    static double[,] ReadImageDataFromFile(string imagePath)
+    {
+       
+        int width = 0, height = 0;
+        double[,] imageData;
+
+        using (StreamReader reader = new StreamReader(imagePath))
+        {
+            
+            string[] size = reader.ReadLine().Split(',');
+            width = int.Parse(size[0]);
+            height = int.Parse(size[1]);
+
+            
+            imageData = new double[height, width];
+
+            
+            for (int i = 0; i < height; i++)
+            {
+                string[] values = reader.ReadLine().Split(',');
+
+                for (int j = 0; j < width; j++)
+                {
+                    imageData[i, j] = double.Parse(values[j]);
+                }
+            }
+        }
+
+       
+
+        
